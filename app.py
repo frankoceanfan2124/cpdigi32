7276ba4b017884a8c3bbd25256b5ba2a8dbc9393
frankoceanfan2124 <stu24200@ashs.school.nz>
2026-09-01 11:55:54 +1200

    Refined all code

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 from flask import Flask, render_template
 
-#  Flask applciatiion
-# make sure it matches my folder structure
 app = Flask(__name__, template_folder='template', static_folder='static')
 
 @app.route('/')
 def home():
-    # this is the thingy for index
     return render_template('index.html')
 
+@app.route('/terms')
+def terms():
+    return render_template('terms.html')
+
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 if __name__ == '__main__':
-   
-    app.run(debug=True)
+    app.run(debug=True) 
